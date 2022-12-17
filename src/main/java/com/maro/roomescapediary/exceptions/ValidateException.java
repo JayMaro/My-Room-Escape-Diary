9b6fc95da81635f8c228a988d423cb53c4ed9027
KimJinkYU <jinkyu1996@hanmail.net>
2022-12-17 19:56:23 +0900

    feat: Refactor ValidateException

--- src/main/java/com/maro/roomescapediary/exceptions/ValidateException.java
+++ src/main/java/com/maro/roomescapediary/exceptions/ValidateException.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 @Getter
 public class ValidateException extends RuntimeException{
 
-    private String message;
-
     public ValidateException(String message) {
         super(message);
     }
