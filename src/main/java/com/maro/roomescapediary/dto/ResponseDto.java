b4ad7ebfb4e3452c7fb305cb5b094b646195d907
KimJinkYU <jinkyu1996@hanmail.net>
2022-12-17 19:55:49 +0900

    feat: Refactor ApiExceptionHandler

---
+++ src/main/java/com/maro/roomescapediary/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.maro.roomescapediary.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+
+    private String message;
+
+}
--- src/main/java/com/maro/roomescapediary/utils/ApiExceptionHandler.java
+++ src/main/java/com/maro/roomescapediary/utils/ApiExceptionHandler.java
@@ -1,29 +1,35 @@
 package com.maro.roomescapediary.utils;
 
+import com.maro.roomescapediary.dto.ResponseDto;
 import com.maro.roomescapediary.exceptions.ValidateException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@RequiredArgsConstructor
+@RestControllerAdvice(annotations = {RestController.class})
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public String handleException(Exception e) {
-        return "내부 오류가 발생했습니다.";
+    public ResponseDto handleException(Exception e) {
+        return new ResponseDto("내부 오류가 발생했습니다.");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ValidateException.class)
-    public String handleValidateException(ValidateException e) {
-        return e.getMessage();
+    public ResponseDto handleValidateException(ValidateException e) {
+        return new ResponseDto(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
-    public String handleIllegalArgumentException(IllegalArgumentException e) {
-        return "요청한 값이 존재하지 않습니다.";
+    public ResponseDto handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseDto("요청한 값이 존재하지 않습니다.");
     }
 
 }
